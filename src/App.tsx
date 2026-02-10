d0f43f4831d55633b73c1bf158cfe6a020c5cb76
Programmer-Dev125 <programmerdev125@gmail.com>
2026-02-10 13:00:44 +0500

    s

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ export default function App(){
                 positions={{
                     ...(current === "about" || current === "projects") ? {
                         left: outWidth / 7,
-                        bottom: screenHeight <= 768 ? -(outHeight / 2) : 30
+                        bottom: screenHeight <= 768 ? -(outHeight / 3) : 30
                     } : {
                         top: menuDimensions.height + (screenWidth <= 900 ? outHeight / 1.2 : (outHeight / 5) / 2),
                         right: outWidth / 4,
